529c34040d86be1db6c7e22b644aa957552711d1
Marek Vedral <marek.vedral@gmail.com>
2023-04-13 12:10:39 +0200

    Stub: Add functions to read and write all regs

--- include/zephyr/arch/arm/aarch32/gdbstub.h
+++ include/zephyr/arch/arm/aarch32/gdbstub.h
@@ -6,22 +6,25 @@
 #define GDB_STUB_NUM_REGISTERS 8
 
 enum AARCH32_GDB_REG {
-    R0 = 0,
-    R1,
-    R2,
-    R3,
-    R12,
-    LR,
-    PC,
-    /* Saved program status register */
-    SPSR
+	R0 = 0,
+	R1,
+	R2,
+	R3,
+	R12,
+	LR,
+	PC,
+	/* Saved program status register */
+	SPSR
 };
+
+/* Position of each register in the packet, see GDB code */
+const int packet_pos[] = {0, 1, 2, 3, 12, 14, 15, 25};
 
 // required structure
 struct gdb_ctx {
-    // cause of the exception
-    unsigned int exception;
-    unsigned int registers[GDB_STUB_NUM_REGISTERS];
+	// cause of the exception
+	unsigned int exception;
+	unsigned int registers[GDB_STUB_NUM_REGISTERS];
 };
 
 static void z_gdb_entry(z_arch_esf_t *esf);
